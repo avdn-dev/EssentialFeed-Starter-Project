9b9590920bc1be69eaa2dc7c19c1ab53bf7d37db
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-12-03 16:13:11 +1100

    Complete "Asserting a Captured Value Is Not Enough + Cross-Module Access Control"

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -7,41 +7,35 @@
 
 import Foundation
 import Testing
-
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
-    
-    init(url: URL, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+import EssentialFeed
 
 struct RemoteFeedLoaderTests {
     @Test("Initialised feed loader does not request data")
     func initialisedFeedLoaderDoesntRequestData() {
         let (_, client) = makeSUT()
         
-        #expect(client.requestedURL == nil)
+        #expect(client.requestedUrls.isEmpty)
     }
     
-    @Test("Feed loader loads request data from URL")
+    @Test("Feed loader loads requests data from URL")
     func feedLoaderRequestsDataFromUrl() {
         let url = URL(string: "https://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
-        #expect(client.requestedURL == url)
+        #expect(client.requestedUrls == [url])
+    }
+    
+    @Test("Feed loader loading twice requests data from URL twice")
+    func feedLoaderRequestsDataFromUrlTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        
+        sut.load()
+        sut.load()
+        
+        #expect(client.requestedUrls == [url, url])
     }
     
     // MARK: Helpers
@@ -52,10 +46,10 @@ struct RemoteFeedLoaderTests {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
+        var requestedUrls = [URL]()
         
         func get(from url: URL) {
-            requestedURL = url
+            requestedUrls.append(url)
         }
     }
 }
