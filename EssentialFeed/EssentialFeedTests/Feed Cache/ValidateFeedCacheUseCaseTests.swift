26e30cdeecd4672c85a9fae9d9c960bba58d21e5
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-12-17 12:09:46 +0700

    Add test that checks LocalFeedLoader does not message store before validation

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Anh Nguyen on 17/12/2024.
+//
+
+import EssentialFeed
+import Foundation
+import Testing
+
+final class ValidateFeedCacheUseCaseTests {
+    private var sutTracker: MemoryLeakTracker<LocalFeedLoader>?
+    private var storeTracker: MemoryLeakTracker<FeedStoreSpy>?
+    
+    deinit {
+        sutTracker?.verifyDeallocation()
+        storeTracker?.verifyDeallocation()
+    }
+    
+    @Test("LocalFeedLoader does not message store upon initialisation")
+    func initialiserDoesNotMessageStore() {
+        let (_, store) = makeSut()
+        
+        #expect(store.receivedMessages == [])
+    }
+    
+    // MARK: Helpers
+    private func makeSut(currentDate: @escaping () -> Date = Date.init, sourceLocation: SourceLocation = #_sourceLocation) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store: store, currentDate: currentDate)
+        sutTracker = MemoryLeakTracker(instance: sut, sourceLocation: sourceLocation)
+        storeTracker = MemoryLeakTracker(instance: store, sourceLocation: sourceLocation)
+        return (sut, store)
+    }
+}
