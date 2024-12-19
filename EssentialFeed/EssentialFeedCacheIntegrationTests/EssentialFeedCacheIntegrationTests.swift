d268512e9b1875e5ebbfef42c31550a168d68393
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-12-19 07:31:37 +0700

    Add EssentialFeedCacheIntegrationTests to separate slower integration tests from unit tests

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Anh Nguyen on 19/12/2024.
+//
+
+import Testing
+
+struct EssentialFeedCacheIntegrationTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
