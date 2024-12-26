6291c6e9625229d1874c7b1cdcca24fb7ce56e4f
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-12-26 19:22:12 +0700

    Add test and implementation for FeedViewController that loads feed on refresh

--- EssentialFeed/EssentialFeedIOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeedIOSTests/FeedViewControllerTests.swift
@@ -9,18 +9,28 @@ import EssentialFeed
 import Testing
 import UIKit
 
-final class FeedViewController: UIViewController {
-    private var loader: FeedLoader!
+final class FeedViewController: UITableViewController {
+    private var loader: FeedLoader?
     
     convenience init(loader: FeedLoader) {
         self.init()
         self.loader = loader
     }
     
+    deinit {
+        loader = nil
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loader.load { _ in }
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+        load()
+    }
+    
+    @objc private func load() {
+        loader?.load { _ in }
     }
 }
 
@@ -50,6 +60,18 @@ final class FeedViewControllerTests {
         #expect(loader.loadCallCount == 1)
     }
     
+    @Test("Pull to refresh loads feed")
+    func pullToRefreshLoadsFeed() {
+        let (sut, loader) = makeSut()
+        sut.loadViewIfNeeded()
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        #expect(loader.loadCallCount == 2)
+        
+        sut.refreshControl?.simulatePullToRefresh()
+        #expect(loader.loadCallCount == 3)
+    }
+    
     // MARK: Helpers
     func makeSut(sourceLocation: SourceLocation = #_sourceLocation) -> (sut: FeedViewController, loader: LoaderSpy) {
         let loader = LoaderSpy()
@@ -64,6 +86,16 @@ final class FeedViewControllerTests {
         
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
             loadCallCount += 1
+        }
+    }
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
         }
     }
 }
