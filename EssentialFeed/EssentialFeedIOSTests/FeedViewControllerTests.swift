803624b2950d41ccda3a1b7eda8bddf92cb29ba9
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2024-12-26 17:50:09 +0700

    Add test and implementation for FeedViewController that loads feed on viewDidLoad

--- EssentialFeed/EssentialFeedIOSTests/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeedIOSTests/FeedViewControllerTests.swift
@@ -6,13 +6,24 @@
 //
 
 import Testing
+import UIKit
 
-final class FeedViewController {
-    init(loader: FeedViewControllerTests.LoaderSpy) {
+final class FeedViewController: UIViewController {
+    private var loader: FeedViewControllerTests.LoaderSpy!
+    
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        loader.load()
     }
 }
 
+@MainActor
 struct FeedViewControllerTests {
     @Test("Initialiser does not load feed")
     func initialiserDoesNotLoadFeed() {
@@ -22,7 +33,21 @@ struct FeedViewControllerTests {
         #expect(loader.loadCallCount == 0)
     }
     
+    @Test("viewDidLoad call loads feed")
+    func viewDidLoadLoadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        #expect(loader.loadCallCount == 1)
+    }
+    
     class LoaderSpy {
         private(set) var loadCallCount = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
